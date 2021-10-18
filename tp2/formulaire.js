348fc8f45a6b694c1c08316fd4b3d10ddc0c99b7
Massika-Laskri <92297815+Massika-Laskri@users.noreply.github.com>
2021-10-18 23:43:58 +0200

    LE TP2

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -1,29 +1,38 @@
 function validation() {
-  const nom = document.getElementById("nom").value;
-  const prenom = document.getElementById("prenom").value;
-  const dateDeNaissance = document.getElementById("dateDeNaissance").value;
-  const adress = document.getElementById("adress").value;
-  const mail = document.getElementById("mail").value;
-  console.log('nom', nom);
+    const nom = document.getElementById("nom").value;
+    const prenom = document.getElementById("prenom").value;
+    const dateDeNaissance = document.getElementById("dateDeNaissance").value;
+    const adress = document.getElementById("adress").value;
+    const mail = document.getElementById("mail").value;
 
-  document.getElementById("error").innerHTML = "";
 
-  if (nom.length < 5) {
-      document.getElementById("error").innerHTML = "La saisie du nom est obligatoire <br>";
-  }
-  if (prenom.length < 5) {
-      document.getElementById("error").innerHTML += "La saisie du prenom est obligatoire<br>";
-  }
-  if (dateDeNaissance.length < 5) {
-      document.getElementById("error").innerHTML += "La saisie de la date de naissance est obligatoire <br>";
-  }
-  if (mail.length < 5) {
-      document.getElementById("error").innerHTML += "La saisie du mail est obligatoire <br>";
-  }
-  if (adress.length < 5) {
-      document.getElementById("error").innerHTML += "La saisie d'adresse est obligatoire <br>";
-  }
+    document.getElementById("error").innerHTML = "";
+    document.getElementById("error").style.display = 'none'
+    document.getElementById("result").style.display = 'none'
 
-  if (document.getElementById("error").innerHTML == "") { document.getElementById("result").innerHTML = "Bienvenue " + document.querySelector("#nom").value; }
 
-}
+    if (nom.length < 5) {
+        document.getElementById("error").innerHTML = "Pour le nom saisissez au moins 5 caractères <br>";
+    }
+    if (prenom.length < 5) {
+        document.getElementById("error").innerHTML += "Pour le prénom saisissez au moins 5 caractères<br>";
+    }
+    if (dateDeNaissance.length < 5) {
+        document.getElementById("error").innerHTML += "la saisie de la date de naissance est obligatoire <br>";
+    }
+    if (mail.length < 5) {
+        document.getElementById("error").innerHTML += "la saisie du mail est obligatoire <br>";
+    }
+    if (adress.length < 5) {
+        document.getElementById("error").innerHTML += "la saisie d'adresse postale est obligatoire <br>";
+    }
+
+    if (document.getElementById("error").innerHTML == "") { 
+        document.getElementById("result").style.display = 'block'
+        document.getElementById("result").innerHTML = "WELCOME " + document.querySelector("#prenom").value;
+     }else{
+        document.getElementById("error").style.display = 'block'
+
+     }
+
+} 
