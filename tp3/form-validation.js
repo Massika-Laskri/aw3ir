68a90503c238a5b7654ad35d6fdf8f07f8b324dc
Massika-Laskri <92297815+Massika-Laskri@users.noreply.github.com>
2021-11-28 18:46:43 +0100

    Update form-validation.js

--- tp3/form-validation.js
+++ tp3/form-validation.js
@@ -33,12 +33,6 @@ window.onload = function () {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
   }
-
-      
-      document.getElementById("error").innerHTML +="<a href=https://www.google.com/maps/place/Tour+Eiffel/@48.8584312,2.2864314,13z/data=!4m8!1m2!2m1!1sParis%3E%3Cimg+width%3D450+src%3Dhttps:%2F%2Fmaps.googleapis.com%2Fmaps%2Fapi%2Fstaticmap%3Fmarkers%3DParis!3m4!1s0x47e66e2964e34e2d:0x8ddca9ee380ef7e0!8m2!3d48.8583701!4d2.2944813 /> </a>";
-
-
-
             if (lnom == 0 && lprenom == 0 && lAdresse == 0 && lmail == 0 && ldate == 0  && lcité == 0 && letat == 0 ) {  
               var myModal = new bootstrap.Modal(document.getElementById('myModal1'));
               myModal.show();
